022bbd6d9e96e962287196f58e8559c8d6e8ef7b
dagoldenburg <dagoldenburg@gmail.com>
2018-01-06 17:40:18 +0100

    Add files via upload

--- 3bvg.js
+++ 3bvg.js
@@ -7,13 +7,24 @@ var expressWs = require('express-ws')(app);
 
 app.get('/', function(req, res){
 
-fs.appendFile("test.txt", "bajs", function(err) {
+fs.appendFile("test.txt", "", function(err) {
     if(err) {
         return console.log(err);
     }
 });
+    console.log("The file was reset!");
+});
 
-    console.log("The file was saved!");
+app.post('/upload', function(res, req) {
+	console.log("hejhejhej")
+  res.on('data', function(msg) {
+  	console.log(msg)
+  	fs.appendFile("test.txt","hej",function(err){
+  		if(err){
+  			return console.log(err);
+  		}
+  	});
+  });
 });
 
 port = 3000;
